51b2853cb963cf51e8d2a287e625b91079b1763e
ddauzen <ddauzen1000@gmail.com>
2026-04-20 19:11:42 +0900

    0420 1911 commit
    - 베이스에 기본 공격
    - 플레이어 컨트롤러 클래스 제작
    - 컨트롤러에 마우스 커서 위치 계산 로직 추가
    - 기본 공격 (코어 타격) 실행 확인을 위한 임시 코어 클래스

---
+++ Source/Omega_Strikers/SM/Luna.cpp
@@ -0,0 +1,82 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Luna.h"
+
+
+// Sets default values
+ALuna::ALuna()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ALuna::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ALuna::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ALuna::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+void ALuna::Ready_CoreHit()
+{
+	
+}
+
+void ALuna::Use_CoreHit()
+{
+	FVector SelfLoc = GetActorLocation();
+	
+}
+
+void ALuna::Ready_PrimarySkill()
+{
+	
+}
+
+void ALuna::Use_PrimarySkill()
+{
+	
+}
+
+void ALuna::Ready_SecondarySkill()
+{
+	
+}
+
+void ALuna::Use_SecondarySkill()
+{
+	
+}
+
+void ALuna::Ready_SpecialSkill()
+{
+	
+}
+
+void ALuna::Use_SpecialSkill()
+{
+	
+}
+
+void ALuna::Ready_Flip()
+{
+	
+}
+
+void ALuna::Use_Flip()
+{
+	
+}
