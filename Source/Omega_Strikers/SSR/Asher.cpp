df4ee3d8469d53bfa63942826c462cc5cb0f4631
ssr5087 <ssr5087@gmail.com>
2026-04-23 17:33:18 +0900

    123
    123

---
+++ Source/Omega_Strikers/SSR/Asher.cpp
@@ -0,0 +1,81 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Asher.h"
+
+
+// Sets default values
+AAsher::AAsher()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AAsher::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AAsher::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AAsher::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+void AAsher::Ready_CoreHit()
+{
+	Super::Ready_CoreHit();
+}
+
+void AAsher::Ready_PrimarySkill()
+{
+	Super::Ready_PrimarySkill();
+}
+
+void AAsher::Ready_SecondarySkill()
+{
+	Super::Ready_SecondarySkill();
+}
+
+void AAsher::Ready_SpecialSkill()
+{
+	Super::Ready_SpecialSkill();
+}
+
+void AAsher::Ready_Flip()
+{
+	Super::Ready_Flip();
+}
+
+void AAsher::Use_CoreHit()
+{
+	Super::Use_CoreHit();
+}
+
+void AAsher::Use_PrimarySkill()
+{
+	Super::Use_PrimarySkill();
+}
+
+void AAsher::Use_SecondarySkill()
+{
+	Super::Use_SecondarySkill();
+}
+
+void AAsher::Use_SpecialSkill()
+{
+	Super::Use_SpecialSkill();
+}
+
+void AAsher::Use_Flip()
+{
+	Super::Use_Flip();
+}
---
+++ Source/Omega_Strikers/SSR/Asher.h
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PlayerBase.h"
+#include "Asher.generated.h"
+
+UCLASS()
+class OMEGA_STRIKERS_API AAsher : public APlayerBase
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AAsher();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+public:
+	virtual void Ready_CoreHit() override;
+	virtual void Ready_PrimarySkill() override;
+	virtual void Ready_SecondarySkill() override;
+	virtual void Ready_SpecialSkill() override;
+	virtual void Ready_Flip() override;
+	
+	virtual void Use_CoreHit() override;
+	virtual void Use_PrimarySkill() override;
+	virtual void Use_SecondarySkill() override;
+	virtual void Use_SpecialSkill() override;
+	virtual void Use_Flip() override;
+};
