67d5d612076b49f54e873396843f5ed0e74be446
ddauzen <ddauzen1000@gmail.com>
2026-04-29 17:39:32 +0900

    0429 1739 commit
    - 애니메이션 적용
    - 로켓 소환 애님 노티파이로 호출
    - 기타 전반적 리팩토링 및 주석 작업

--- Source/Omega_Strikers/SM/Luna_PrimaryRocket.cpp
+++ Source/Omega_Strikers/SM/Luna_PrimaryRocket.cpp
@@ -3,9 +3,12 @@
 
 #include "Luna_PrimaryRocket.h"
 
+#include "Luna.h"
+#include "PlayerBase.h"
 #include "Components/BoxComponent.h"
 #include "Core/CoreBall.h"
 #include "Omega_Strikers/Omega_Strikers.h"
+#include "Omega_Strikers/SSR/CharacterSkill.h"
 
 
 // Sets default values
@@ -35,9 +38,38 @@ void ALuna_PrimaryRocket::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	
+	// 소환 후 0.35초 이후에 가속, Impact Data도 같이 갱신
 	FTimerHandle SpeedChanger;
-	GetWorld()->GetTimerManager().SetTimer(SpeedChanger, [this]()->void {Speed = 5000.f;}, 0.35f, false);
+	GetWorld()->GetTimerManager().SetTimer(SpeedChanger,
+		[this]()->void
+		{
+			// 가속
+			Speed = 5000.f;
+			
+			// 데이터 셋 불러오기
+			// Owner 정보 실패 시 방어 코드
+			if (!IsValid(OwnerActorRef)) {return;}
+			
+			// 성공 시 데이터 셋 불러오기
+			FCharacterSkill* Skill = OwnerActorRef->GetSkillData(FName(TEXT("Luna_Primary_Accelerated")));
+			
+			// 데이터 셋 로드 실패 시 리턴
+			if (!Skill) {return;}
+			
+			// ImpactData를 데이터 셋으로부터 계산하여 설정
+			ImpactData = OwnerActorRef->MakeImpactData(*Skill);
+			ImpactData.Direction = OwnerActorRef->PrimaryDir;
+			
+		}, 0.35f, false);
+	
+	
 	// 타이머 써서 몇 초 뒤에 사라지게 or 숨겨놓고 충돌 다 꺼놓고 다음에 소환할 때 또?(x) 그러면 결국 Luna에서도 몇 개 뜯어 고쳐야 함 나아아아ㅏ중에 리팩토링 할 시간 있으면 그 때 하자
+	GetWorld()->GetTimerManager().SetTimer(DestroyTimer,
+		[this]()->void
+		{
+			this->Destroy();
+		}, 10.f, false);
 }
 
 // Called every frame
@@ -48,24 +80,30 @@ void ALuna_PrimaryRocket::Tick(float DeltaTime)
 	SetActorLocation(GetActorLocation() + GetActorForwardVector() * DeltaTime * Speed);
 }
 
-void ALuna_PrimaryRocket::InitRocket(float Owner_Power, AActor* InOwnerActor, EOSTeam InTeamSide)
+void ALuna_PrimaryRocket::InitRocket(AActor* InOwnerActor)
 {
-	Luna_Power = Owner_Power;
-	OwnerActorRef = InOwnerActor;
+	// 데이터 셋 불러올 PlayerBase(Luna)로 캐스팅
+	OwnerActorRef = Cast<ALuna>(InOwnerActor);
 	
-	// 데이터 적용 필요
-	ImpactData.TeamSide = InTeamSide;
-	ImpactData.Direction = FVector2D(GetActorForwardVector().X, GetActorForwardVector().Y).GetSafeNormal();
-	ImpactData.CoreKnockbackPower = 3390.f;
-	ImpactData.PlayerKnockbackPower = 1500.f;
-	ImpactData.PlayerDamage = 100.0f;
+	// 실패 시 리턴
+	if (!IsValid(OwnerActorRef)) {return;}
+	
+	// 성공 시 데이터 셋 불러오기
+	FCharacterSkill* Skill = OwnerActorRef->GetSkillData(FName(TEXT("Luna_Primary_CloseRange")));
+	
+	// 데이터 셋 로드 실패 시 리턴
+	if (!Skill) {return;}
+	
+	// ImpactData를 데이터 셋으로부터 계산하여 설정
+	ImpactData = OwnerActorRef->MakeImpactData(*Skill);
+	ImpactData.Direction = OwnerActorRef->PrimaryDir;
 }
 
 // 충돌 시 데미지 전달해주는 로직 넣기
 void ALuna_PrimaryRocket::OnRocketOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (OtherActor == OwnerActorRef) {return;}
+	if (OtherActor == this || OtherActor == OwnerActorRef) {return;}
 	// OtherActor가 존재하고 그 놈이 인터페이스 구현했으면
 	if (OtherActor && OtherActor->Implements<UOSImpactReceiver>())
 	{
@@ -74,6 +112,14 @@ void ALuna_PrimaryRocket::OnRocketOverlap(UPrimitiveComponent* OverlappedCompone
 		
 		if (bIsSuccess)
 		{
+			LOG_SM_E(TEXT("%f, %f"), ImpactData.PlayerDamage, ImpactData.PlayerKnockbackPower);
+			
+			// 파괴 이후에 BeginPlay에서 돌린 타이머가 살아있으면 오류나니까 미리 해제해주기
+			if (GetWorld()->GetTimerManager().IsTimerActive(DestroyTimer))
+			{
+				GetWorld()->GetTimerManager().ClearTimer(DestroyTimer);
+			}
+			
 			this->Destroy();
 		}
 	}
