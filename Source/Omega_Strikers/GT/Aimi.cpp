59e1b71f0d1102f6203bca03398b8e8c3907cded
ddauzen <ddauzen1000@gmail.com>
2026-05-15 19:30:05 +0900

    0515 1929 commit
    - 아이미 UI만 붙던 문제 해결

--- Source/Omega_Strikers/GT/Aimi.cpp
+++ Source/Omega_Strikers/GT/Aimi.cpp
@@ -112,6 +112,9 @@ void AAimi::BeginPlay()
 		false
 		);
 	
+	// 내가 조종 중인 캐릭터일 때만
+	if (!IsLocallyControlled()) {return;}
+	
 	// UI 붙이기
 	SkillUI = CreateWidget<ULunaSkillCool>(GetWorld(), CoolTimeUI);
 	if (SkillUI)
